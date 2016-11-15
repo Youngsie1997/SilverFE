c68c03c8bb624055fdbb4ad8328e5a87aa9556d1
Youngsie1997 <Connormyoungs@gmail.com>
2016-11-15 17:40:50 +0000

    Added Login System

--- SilverFE/Login.xaml.cs
+++ SilverFE/Login.xaml.cs
@@ -1,18 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Data;
 using MahApps.Metro.Controls;
+using Npgsql;
+
 
 namespace SilverFE
 {
@@ -26,24 +18,125 @@ namespace SilverFE
             InitializeComponent();
         }
 
-        private void LoginWindow_Loaded(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void tbPassword_PreviewKeyDown(object sender, KeyEventArgs e)
-        {
-
-        }
-
-        private void tbUserName_PreviewKeyDown(object sender, KeyEventArgs e)
-        {
-
-        }
+      
 
         private void btLogin_Click(object sender, RoutedEventArgs e)
         {
 
         }
+
+
+        
+        private void login()
+        {
+            if(tbUserName.Text == "" || tbPassword.Password == "")
+            {
+                MessageBox.Show("Please provide Username and Password", "Login", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            try
+            {
+                NpgsqlConnection conn = new NpgsqlConnection("Server=murky.freeborough.com;Port=5432;User Id=connor;Password=poop123;Database=silver;");
+                conn.Open();
+                string sql = "SELECT * FROM Login WHERE UserName='" + tbUserName.Text + "' and Password='" + tbPassword.Password + "'";
+                NpgsqlDataAdapter da = new NpgsqlDataAdapter(sql, conn);
+                DataSet ds = new DataSet();
+                ds.Reset();
+                da.Fill(ds);
+                int count = ds.Tables[0].Rows.Count;
+                conn.Close();
+                if(count == 1)
+                {
+                    MessageBox.Show("Login Successful", "Login", MessageBoxButton.OK, MessageBoxImage.None);
+                    Main main = new Main();
+                    main.Show();
+                    this.Close();
+                }
+                else
+                {
+                   
+                }
+            }
+            catch(Exception Error)
+            {
+                MessageBox.Show(Error.ToString());
+            }
+           
+            
+           
+            
+            
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        #region Misc
+
+        private void LoginWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            tbUserName.Focus();
+            tbUserName.MaxLength = 20;
+            tbPassword.MaxLength = 20;
+        }
+
+
+
+        private void tbUserName_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Space)
+            {
+                e.Handled = true;
+            }
+
+            if(e.Key == Key.Enter)
+            {
+                tbPassword.Focus();
+            }
+        }
+
+        private void tbPassword_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Space)
+            {
+                e.Handled = true;
+            }
+
+            if(e.Key == Key.Return)
+            {
+                login();
+            }
+        }
+        #endregion
+
+
+
+
+
+
+
     }
 }
