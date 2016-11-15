43723f9f719470b03c5125bfc432d532098ff22c
Youngsie1997 <Connormyoungs@gmail.com>
2016-11-15 17:05:43 +0000

    Styled Login Window

--- SilverFE/Login.xaml.cs
+++ SilverFE/Login.xaml.cs
@@ -12,17 +12,38 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
 
 namespace SilverFE
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class Login : Window
+    public partial class Login : MetroWindow
     {
         public Login()
         {
             InitializeComponent();
+        }
+
+        private void LoginWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void tbPassword_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void tbUserName_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void btLogin_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
